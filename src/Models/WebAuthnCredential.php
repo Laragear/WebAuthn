c2e3cce8584099e105c2fb013d36804004e36d79
Italo Israel Baeza Cabrera <DarkGhostHunter@Gmail.com>
2025-02-16 02:04:35 -0300

    Fixes static analysis.

--- src/Models/WebAuthnCredential.php
+++ src/Models/WebAuthnCredential.php
@@ -64,6 +64,8 @@ use const PHP_URL_HOST;
  *
  * @method \Illuminate\Database\Eloquent\Builder|static whereEnabled()
  * @method \Illuminate\Database\Eloquent\Builder|static whereDisabled()
+ *
+ * @phpstan-consistent-constructor
  */
 class WebAuthnCredential extends Model
 {
@@ -211,7 +213,7 @@ class WebAuthnCredential extends Model
             // on the Assertion procedure as the device returns which credential it used.
             $table->string('id', 510)->primary();
 
-            $this->createMorph($table, 'authenticatable', 'webauthn_user_index');
+            $this->createMorph($table, 'authenticatable', 'webauthn_user_index'); // @phpsan-ignore-line
 
             // When requesting to create a credential, the app will set a "user handle" to be
             // a UUID to anonymize the user personal information. If a second credential is
