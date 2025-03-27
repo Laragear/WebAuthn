f64cd27cc59fa2af7b1ada7a77b805f33cb751a1
Italo Israel Baeza Cabrera <DarkGhostHunter@Gmail.com>
2025-03-27 14:23:45 -0300

    Fixes variable initialization [skip ci]

--- src/Http/Requests/AssertedRequest.php
+++ src/Http/Requests/AssertedRequest.php
@@ -85,7 +85,7 @@ class AssertedRequest extends FormRequest
         bool $destroySession,
     ): ?WebAuthnAuthenticatable {
         if (! method_exists($guard, 'attemptWhen')) {
-            $name ??= config('auth.defaults.guard');
+            $name = config('auth.defaults.guard');
             throw new UnexpectedValueException("The [$name] guard does not support attempt callbacks.");
         }
 
