e740bb16ad9f23a92376ba2a0795ad9606e9ebef
Italo Israel Baeza Cabrera <DarkGhostHunter@Gmail.com>
2026-04-06 17:55:19 -0400

    Fixes challenge serialization and adds tests.

--- src/Challenge/Challenge.php
+++ src/Challenge/Challenge.php
@@ -3,10 +3,14 @@
 namespace Laragear\WebAuthn\Challenge;
 
 use DateTimeInterface;
+use Illuminate\Contracts\Support\Arrayable;
+use Illuminate\Contracts\Support\Jsonable;
 use Illuminate\Support\Facades\Date;
+use JsonSerializable;
 use Laragear\WebAuthn\ByteBuffer;
+use function json_encode;
 
-class Challenge
+class Challenge implements JsonSerializable, Arrayable, Jsonable
 {
     /**
      * Create a new Challenge instance.
@@ -18,7 +22,10 @@ class Challenge
         public array $properties = [],
         public int $expiresAt = 0,
     ) {
-        $this->expiresAt = Date::now()->getTimestamp() + $this->timeout;
+        // If the expiration timestamp was set, use that, otherwise initialize it based on the timeout.
+        if ($this->expiresAt < 1) {
+            $this->expiresAt = Date::now()->getTimestamp() + $this->timeout;
+        }
     }
 
     /**
@@ -42,7 +49,7 @@ class Challenge
      */
     public function isExpired(): bool
     {
-        return ! $this->isValid();
+        return !$this->isValid();
     }
 
     /**
@@ -53,8 +60,59 @@ class Challenge
         return new static(ByteBuffer::makeRandom($length), $timeout, $verify, $options);
     }
 
+    /**
+     * Creates a new Challenge instance using a binary string and timeout.
+     */
     public static function make(string $binary, int $timeout): static
     {
         return new static(new ByteBuffer($binary), $timeout);
+    }
+
+    /**
+     * @inheritDoc
+     *
+     * @return array{data: string, timeout: int, verify: bool, properties: array, expires_at: int}
+     */
+    public function toArray(): array
+    {
+        return [
+            'data' => $this->data->toHex(),
+            'timeout' => $this->timeout,
+            'verify' => $this->verify,
+            'properties' => $this->properties,
+            'expires_at' => $this->expiresAt,
+        ];
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function jsonSerialize(): array
+    {
+        return $this->toArray();
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function toJson($options = 0): string
+    {
+        return json_encode($this->jsonSerialize(), $options);
+    }
+
+    /**
+     * Creates a new instance from an array.
+     *
+     * @param  array{data: string, timeout: int, verify: bool, properties: array, expires_at: int}  $array
+     */
+    public static function fromArray(array $array): static
+    {
+        return new static(
+            ByteBuffer::fromHex($array['data']),
+            $array['timeout'],
+            $array['verify'],
+            $array['properties'],
+            $array['expires_at'],
+        );
     }
 }
--- src/Challenge/SessionChallengeRepository.php
+++ src/Challenge/SessionChallengeRepository.php
@@ -9,6 +9,7 @@ use Laragear\WebAuthn\Assertion\Validator\AssertionValidation;
 use Laragear\WebAuthn\Attestation\Creator\AttestationCreation;
 use Laragear\WebAuthn\Attestation\Validator\AttestationValidation;
 use Laragear\WebAuthn\Contracts\WebAuthnChallengeRepository;
+use function is_array;
 
 /**
  * @internal
@@ -38,8 +39,12 @@ class SessionChallengeRepository implements WebAuthnChallengeRepository
      */
     public function pull(AttestationValidation|AssertionValidation $ceremony): ?Challenge
     {
-        /** @var \Laragear\WebAuthn\Challenge\Challenge|null $challenge */
+        /** @var \Laragear\WebAuthn\Challenge\Challenge|array|null $challenge */
         $challenge = $this->session->pull($this->config->get('webauthn.challenge.key'));
+
+        if (is_array($challenge)) {
+            $challenge = Challenge::fromArray($challenge);
+        }
 
         // Only return the challenge if it's valid (not expired)
         if ($challenge?->isValid()) {
